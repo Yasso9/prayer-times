01159e6eb05f636e0d39d6e1f979521d5aac2706
Ilyas Turki <turki.ilyass@gmail.com>
2024-01-11 16:30:40 +0100

    fajr and ishaa calculation

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,11 @@
-mod time;
-use crate::time::prayer_time::{EnumPrayer, Prayers};
-use chrono::{Local, NaiveDate};
-use clap::Parser;
+mod cli;
+mod prayers;
 
+use chrono::Local;
+use notify_rust::{Notification, Urgency};
 use std::path::PathBuf;
 
-use islam::salah::{Config, Location, Madhab, Method, PrayerSchedule};
-use notify_rust::{Notification, Urgency};
+use crate::prayers::{EnumPrayer, PrayersSchedule};
 
 // Get the icon of the notification that should be sent
 fn get_icon() -> Result<PathBuf, std::io::Error> {
@@ -29,160 +28,26 @@ fn send_notification(prayer_name: &str) {
     }
 }
 
-#[derive(Debug, Clone)]
-enum PrayerMethod {
-    Karachi,
-    MuslimWorldLeague,
-    Egyptian,
-    UmmAlQura,
-    NorthAmerica,
-    French,
-    Singapore,
-    Russia,
-    FixedInterval,
-}
-impl Default for PrayerMethod {
-    fn default() -> Self {
-        PrayerMethod::MuslimWorldLeague
-    }
-}
-impl From<&str> for PrayerMethod {
-    fn from(value: &str) -> Self {
-        match value {
-            "Karachi" => PrayerMethod::Karachi,
-            "MuslimWorldLeague" => PrayerMethod::MuslimWorldLeague,
-            "Egyptian" => PrayerMethod::Egyptian,
-            "UmmAlQura" => PrayerMethod::UmmAlQura,
-            "NorthAmerica" => PrayerMethod::NorthAmerica,
-            "French" => PrayerMethod::French,
-            "Singapore" => PrayerMethod::Singapore,
-            "Russia" => PrayerMethod::Russia,
-            "FixedInterval" => PrayerMethod::FixedInterval,
-            _ => {
-                println!(
-                    "Invalid method: {value}, using default: {:?}",
-                    PrayerMethod::default()
-                );
-                PrayerMethod::default()
-            }
-        }
-    }
-}
-impl From<PrayerMethod> for Method {
-    fn from(prayer_method: PrayerMethod) -> Self {
-        match prayer_method {
-            PrayerMethod::Karachi => Method::Karachi,
-            PrayerMethod::MuslimWorldLeague => Method::MuslimWorldLeague,
-            PrayerMethod::Egyptian => Method::Egyptian,
-            PrayerMethod::UmmAlQura => Method::UmmAlQura,
-            PrayerMethod::NorthAmerica => Method::NorthAmerica,
-            PrayerMethod::French => Method::French,
-            PrayerMethod::Singapore => Method::Singapore,
-            PrayerMethod::Russia => Method::Russia,
-            PrayerMethod::FixedInterval => Method::FixedInterval,
-        }
-    }
-}
-#[derive(Debug, Clone)]
-enum PrayerMadhab {
-    Hanafi,
-    Shafi,
-}
-impl Default for PrayerMadhab {
-    fn default() -> Self {
-        PrayerMadhab::Hanafi
-    }
-}
-impl From<&str> for PrayerMadhab {
-    fn from(value: &str) -> Self {
-        match value {
-            "Hanafi" => PrayerMadhab::Hanafi,
-            "Shafi" => PrayerMadhab::Shafi,
-            _ => {
-                println!(
-                    "Invalid method: {value}, using default: {:?}",
-                    PrayerMadhab::default()
-                );
-                PrayerMadhab::default()
-            }
-        }
-    }
-}
-impl From<PrayerMadhab> for Madhab {
-    fn from(madhab: PrayerMadhab) -> Self {
-        match madhab {
-            PrayerMadhab::Hanafi => Madhab::Hanafi,
-            PrayerMadhab::Shafi => Madhab::Shafi,
-        }
-    }
-}
-
-/// Program to notify prayer times
-#[derive(Parser, Debug)]
-#[command(name = "prayer-time")]
-// Read from `Cargo.toml`
-#[command(author, version, about, long_about = None)]
-struct Args {
-    /// Calculation Method to use
-    #[arg(short = 'm', long, default_value = "MuslimWorldLeague")]
-    method: PrayerMethod,
-    #[arg(short = 'M', long, default_value = "Hanafi")]
-    madhab: PrayerMadhab,
-
-    /// Minutes to add or remove to the Fajr time
-    #[arg(long, default_value_t = 0)]
-    fajr_mod: u8,
-    /// Minutes to add or remove to the Dohr time
-    #[arg(long, default_value_t = 0)]
-    dohr_mod: u8,
-    /// Minutes to add or remove to the Asr time
-    #[arg(long, default_value_t = 0)]
-    asr_mod: u8,
-    /// Minutes to add or remove to the Maghrib time
-    #[arg(long, default_value_t = 0)]
-    maghrib_mod: u8,
-    /// Minutes to add or remove to the Isha time
-    #[arg(long, default_value_t = 0)]
-    isha_mod: u8,
-}
-
 fn main() {
     println!("Starting Prayer Time Daemon");
 
-    let args = Args::parse();
+    let args = crate::cli::parse_cli();
     println!("name: {:?}", args);
 
-    let method: Method = Method::from(args.method);
-    let madhab: Madhab = Madhab::from(args.madhab);
     const SLEEPING_TIME: std::time::Duration = std::time::Duration::from_secs(30);
 
-    let current_location = Location::new(49.049182, 2.035162);
-    let config = Config::new().with(method, madhab);
-    let prayer_times = PrayerSchedule::new(current_location)
-        .expect("Failed to create prayer times. Maybe wrong location?")
-        .with_config(config)
-        .calculate()
-        .expect("Failed to calculate prayer times");
+    let prayers = PrayersSchedule::new(Local::now().date_naive(), 49.049182, 2.035162);
+    println!("Fajr: {}", prayers.get(EnumPrayer::Fajr).date());
+    println!("Dhuhr: {}", prayers.get(EnumPrayer::Dhuhr).date());
+    // println!("Asr: {}", prayers.get(EnumPrayer::Asr).date());
+    // println!("Maghrib: {}", prayers.get(EnumPrayer::Maghrib).date());
+    println!("Isha: {}", prayers.get(EnumPrayer::Isha).date());
 
     // Test notification
     // send_notification("Asr");
 
-    let prayers = Prayers::new(Local::now().date_naive(), 49.049182, 2.035162);
-    println!("Dhuhr: {}", prayers.get(EnumPrayer::Dhuhr).date());
-
-    // let mut prayer = prayer_times.current();
-    // loop {
-    //     println!("Checking prayer time...");
-
-    //     let new_prayer = prayer_times.current();
-    //     // Check if the prayer has changed
-    //     if new_prayer != prayer {
-    //         let prayer_name = new_prayer.name().unwrap_or_default();
-    //         send_notification(&prayer_name);
-    //         prayer = new_prayer;
-    //     }
-
-    //     println!("Sleeping...");
-    //     std::thread::sleep(SLEEPING_TIME);
-    // }
+    loop {
+        println!("Sleeping...");
+        std::thread::sleep(SLEEPING_TIME);
+    }
 }
---
+++ src/prayers/calculations.rs
@@ -0,0 +1,81 @@
+use chrono::{Datelike, NaiveDate};
+
+fn fix(a: f64, b: f64) -> f64 {
+    let result = a - b * (a / b).floor();
+    if result < 0. {
+        result + b
+    } else {
+        result
+    }
+}
+
+mod math {
+    pub(crate) fn dsin(degrees: f64) -> f64 {
+        degrees.to_radians().sin()
+    }
+
+    pub(crate) fn dcos(degrees: f64) -> f64 {
+        degrees.to_radians().cos()
+    }
+
+    pub(crate) fn darcsin(x: f64) -> f64 {
+        x.asin().to_degrees()
+    }
+
+    pub(crate) fn darccos(x: f64) -> f64 {
+        x.acos().to_degrees()
+    }
+
+    pub(crate) fn darctan2(y: f64, x: f64) -> f64 {
+        y.atan2(x).to_degrees()
+    }
+}
+
+// https://orbital-mechanics.space/reference/julian-date.html
+fn to_julian_day(date: NaiveDate) -> f64 {
+    let day = date.day() as f64;
+    let month = date.month() as f64;
+    let year = date.year() as f64;
+
+    let a = (month - 14.) / 12.;
+    let b = 1461. * (year + 4800. + a);
+    let c = 367. * (month - 2. - (12. * a));
+    let e = (year + 4900. + a) / 100.;
+
+    (b / 4.) + (c / 12.) - (3. * e / 4.) + day - 32075.
+}
+
+struct DayValues {
+    dos: f64,
+    eot: f64,
+}
+impl DayValues {
+    fn new(date: NaiveDate) -> DayValues {
+        let jd = to_julian_day(date);
+
+        let d = jd - 2451545.0;
+
+        let g = fix(357.529 + 0.98560028 * d, 360.);
+        let q = fix(280.459 + 0.98564736 * d, 360.);
+        let l = fix(q + 1.915 * math::dsin(g) + 0.020 * math::dsin(2. * g), 360.);
+        let e = 23.439 - 0.00000036 * d;
+        let ra = math::darctan2(math::dcos(e) * math::dsin(l), math::dcos(l)) / 15.;
+
+        let dos = math::darcsin(math::dsin(e) * math::dsin(l));
+        let eot = q / 15. - fix(ra, 24.);
+        DayValues { dos, eot }
+    }
+}
+
+pub fn solar_time_adjustment(date: NaiveDate, angle: f64, latitude: f64) -> f64 {
+    let dos = DayValues::new(date).dos;
+    let a = -math::dsin(angle) - math::dsin(latitude) * math::dsin(dos);
+    let b = math::dcos(latitude) * math::dcos(dos);
+
+    let sta = 1. / 15. * math::darccos(a / b);
+    sta
+}
+
+pub fn equation_of_time(date: NaiveDate) -> f64 {
+    DayValues::new(date).eot
+}
