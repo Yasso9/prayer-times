06cb35b9d09ce40371ae561eb51f1daf45d0501a
Ilyas Turki <turki.ilyass@gmail.com>
2024-02-18 14:07:26 +0100

    Fix double notification bug + notification after suspend

--- src/main.rs
+++ src/main.rs
@@ -1,57 +1,56 @@
+mod calculations;
+mod config;
+mod event;
 mod notification;
+mod prayer;
 mod prayers;
 
-use chrono::Duration;
-
-use crate::{
+use self::{
+    config::Config,
     notification::{notify_before_prayer, notify_prayer},
-    prayers::arguments::Config,
 };
 
 fn main() {
     println!("Starting Prayer Time Daemon");
-
-    // Print Prayer Informations
-    for prayer in prayers::list_prayers() {
-        println!("Adhan {} at {}", prayer.event(), prayer.date_time().time());
-    }
-    let current_prayer = prayers::current();
-    println!(
-        "Current {} since {}",
-        current_prayer.event(),
-        current_prayer.date_time().time()
-    );
-
-    let mut next_prayer = prayers::next();
-    println!(
-        "{} at {}",
-        next_prayer.event(),
-        next_prayer.date_time().time()
-    );
+    println!("Waiting for next prayer...");
 
     let config = Config::new();
 
+    println!("\nPrayer times:");
+    for prayer in prayers::list_prayers(&config) {
+        println!("{}", prayer.text_time());
+    }
+
+    let mut next_prayer = prayers::next(&config);
+
+    // For test purpose
+    if cfg!(debug_assertions) {
+        notify_prayer(&next_prayer);
+    }
+
     let mut is_notified_before = false;
-
     loop {
-        let time_remaining = next_prayer.time_remaining();
-        println!(
-            "Time remaining for {} is {:02}:{:02}",
-            next_prayer.event(),
-            time_remaining.num_minutes() / 60,
-            time_remaining.num_minutes() % 60
-        );
+        println!("{}", next_prayer.text_duration());
+        println!("{}", next_prayer.text_time());
 
-        if config.notify_before() && !is_notified_before && time_remaining < Duration::minutes(10) {
-            notify_before_prayer(&next_prayer, time_remaining);
-            is_notified_before = true;
-        }
+        if next_prayer.time_has_passed() {
+            println!("Prayer time has passed");
+            // Notification only if it's the current prayer.
+            // If it's not the current prayer, it means that the system have been suspended
+            // so we Are currently in an other prayer
+            if next_prayer == prayers::current(&config) {
+                notify_prayer(&next_prayer);
+            }
 
-        if time_remaining <= Duration::zero() {
-            notify_prayer(&next_prayer);
             // Update next prayer
-            next_prayer = prayers::next();
+            next_prayer = prayers::next(&config);
             is_notified_before = false;
+        } else if config.notify_before()
+            && !is_notified_before
+            && next_prayer.time_remaining() < chrono::Duration::minutes(11)
+        {
+            notify_before_prayer(&next_prayer, next_prayer.time_remaining());
+            is_notified_before = true;
         }
 
         println!("Sleeping...");
---
+++ src/prayer.rs
@@ -0,0 +1,125 @@
+use crate::calculations::{asr, equation_of_time, solar_time_adjustment};
+use crate::event::Event;
+use crate::Config;
+use chrono::{Days, Duration, Local, NaiveDate, NaiveDateTime, NaiveTime};
+
+pub fn get_prayer(enum_prayer: Event, date: NaiveDate, config: &Config) -> Prayer {
+    // Time of dhuhr. Used to calculate all other prayers
+    fn dhuhr(date: NaiveDate, config: &Config) -> f64 {
+        let timezone = Local::now().offset().local_minus_utc() / 3600;
+        let a = 12 + timezone;
+        let b = config.lon() / 15.;
+        let c = equation_of_time(date);
+        a as f64 - b - c
+    }
+    fn to_naive_date_time(date: NaiveDate, time: f64) -> NaiveDateTime {
+        let naive_time = NaiveTime::from_num_seconds_from_midnight_opt((time * 3600.) as u32, 0);
+        NaiveDateTime::new(date, naive_time.expect("Error in prayer calculation"))
+    }
+
+    // let config = Config::new();
+    let dhuhr = dhuhr(date, &config);
+
+    let time;
+
+    match enum_prayer {
+        Event::Fajr => {
+            time = dhuhr - solar_time_adjustment(date, config.lat(), config.fajr())
+                + config.fajr_offset();
+        }
+        Event::Dhuhr => {
+            time = dhuhr + config.dhuhr_offset();
+        }
+        Event::Asr => {
+            time =
+                dhuhr + asr(date, config.lat(), config.shadow_multiplier()) + config.asr_offset();
+        }
+        Event::Maghrib => {
+            let sunset = dhuhr + solar_time_adjustment(date, config.lat(), 0.833);
+            time = sunset + config.maghrib_offset();
+        }
+        Event::Isha => {
+            time = dhuhr
+                + solar_time_adjustment(date, config.lat(), config.isha())
+                + config.isha_offset();
+        }
+    }
+
+    Prayer {
+        event: enum_prayer,
+        date: to_naive_date_time(date, time),
+    }
+}
+
+#[derive(PartialEq)]
+pub struct Prayer {
+    event: Event,
+    date: NaiveDateTime,
+}
+impl Prayer {
+    pub fn event(&self) -> Event {
+        self.event
+    }
+    pub fn date_time(&self) -> NaiveDateTime {
+        self.date
+    }
+
+    pub fn previous(&self, config: &Config) -> Prayer {
+        let previous_prayer = get_prayer(self.event.previous(), self.date.date(), config);
+        if previous_prayer.date_time().time() <= self.date_time().time() {
+            return previous_prayer;
+        }
+
+        let previous_date = self
+            .date_time()
+            .date()
+            .checked_sub_days(Days::new(1))
+            .unwrap();
+        return get_prayer(self.event.previous(), previous_date, config);
+    }
+
+    pub fn next(&self, config: &Config) -> Prayer {
+        let next_prayer = get_prayer(self.event.next(), self.date.date(), config);
+        if next_prayer.date_time().time() >= self.date_time().time() {
+            return next_prayer;
+        }
+
+        let next_date = self
+            .date_time()
+            .date()
+            .checked_add_days(Days::new(1))
+            .unwrap();
+        return get_prayer(self.event.next(), next_date, config);
+    }
+
+    // Returns the time remaining for the next prayer to happen
+    pub fn time_remaining(&self) -> Duration {
+        let duration = self
+            .date_time()
+            .signed_duration_since(Local::now().naive_local());
+
+        // The time remaining should not be negative
+        if duration < Duration::zero() {
+            return Duration::zero();
+        }
+        duration
+    }
+
+    // Returns true if the time of the prayer passed
+    pub fn time_has_passed(&self) -> bool {
+        self.time_remaining() <= Duration::zero()
+    }
+
+    pub fn text_duration(&self) -> String {
+        let time_remaining = self.time_remaining();
+        format!(
+            "Adhan {} in {:02}:{:02}",
+            self.event(),
+            time_remaining.num_hours(),
+            time_remaining.num_minutes() % 60
+        )
+    }
+    pub fn text_time(&self) -> String {
+        format!("Adhan {} at {}", self.event(), self.date_time().time())
+    }
+}
